8ad81b25f6c784d38df93e072eeb45c642118791
Chia <lovemaple0913@gmail.com>
2021-10-08 09:38:06 +0800

    [ADD]image-webpack-loader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const IN_PRODUCTION = process.env.NODE_ENV === "production";
+
 module.exports = {
   devServer: {
     port: 8800
@@ -6,19 +8,19 @@ module.exports = {
     loaderOptions: {
       // 向所有 Sass 樣式傳入共享的全局變量
       scss: {
-        prependData: '@import "@/assets/css/base/index.scss";',
-      },
-    },
+        prependData: '@import "@/assets/css/base/index.scss";'
+      }
+    }
   },
-  //   chainWebpack: config => {
-  //     // image-webpack-loader : compress image
-  //     config.module
-  //       .rule('images')
-  //       .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
-  //       .use('image-webpack-loader')
-  //       .loader('image-webpack-loader')
-  //       .options({
-  //         disable: process.env.NODE_ENV !== 'production'
-  //       })
-  //   }
+  chainWebpack: (config) => {
+    // image-webpack-loader : compress image
+    IN_PRODUCTION && config.module
+      .rule("images")
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use("image-webpack-loader")
+      .loader("image-webpack-loader")
+      .options({
+        disable: process.env.NODE_ENV !== "production"
+      });
+  }
 };
